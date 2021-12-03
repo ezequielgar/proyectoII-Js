577c5e65f2e32e6cffa8211218d6b6f7686355d7
Pablo Romero Abadie <pabloabadie1995@gmail.com>
2021-12-03 08:57:07 -0300

    agregue la estructura basica html de la pagina administracion y almacene los input en variables en un archivo js

---
+++ js/admin.js
@@ -0,0 +1,7 @@
+//aqui agrego los eventos a los elementos del formulario
+
+let campoProducto = document.querySelector("#producto");
+let campoCodigo = document.querySelector("#codigo");
+let campoDescripcion = document.querySelector("#descripcion");
+let campoURL = document.querySelector ("#url");
+let campoPrecio = document.querySelector("#precio");
