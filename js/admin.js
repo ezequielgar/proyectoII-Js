7ba854ceec48cc9220185c43ae92506272e4322e
Pablo Romero Abadie <pabloabadie1995@gmail.com>
2021-12-06 09:02:28 -0300

    agregue la funcion validar general para verificar que esten bien cargados todos los datos por el usuario o administrador

--- js/admin.js
+++ js/admin.js
@@ -1,34 +1,36 @@
 // creo la funcion para que cada vez que se cargue la pagina se genere un numero aleatorio unico en el input del codigo
 window.onload = main;
 function main(){
-    campoCodigo.value = Math.floor(Math.random() * 100);
+    campoCodigo.value = Math.floor(Math.random() * 1000);
 }
 
 function campoRequerido(input){
     if(input.value.trim().length > 0){
         input.className = "form-control is-valid";
+        return true;
     }else{
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 function campoRequeridoDescripcion(input){
     if(input.value.trim().length > 5){
         input.className = "form-control is-valid";
+        return true;
     }else{
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 
 function ValidarNumeros(input){
-    // crear una expresion regular
     let patron = /^[0-9]{1,8}$/;
-    //probar el funcionamiento del patron o expresion regular
     if(patron.test(input.value)){
-        //cumple la expresion regular
         input.className = "form-control is-valid";
+        return true;
     }else{
-        //si no cumple la expresion regular
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 
@@ -36,8 +38,25 @@ function ValidarURL(input){
     let patron = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/
     if(patron.test(input.value)){
         input.className = "form-control is-valid";
+        return true;
     }else{
         input.className = "form-control is-invalid";
+        return false;
+    }
+}
+
+function ValidarGeneral(e){
+    e.preventDefault();
+    let alerta = document.querySelector("#msjAlerta");
+    if(campoRequerido(campoProducto) &&
+    campoRequeridoDescripcion(campoDescripcion) && 
+    ValidarURL(campoURL) && 
+    ValidarNumeros(campoPrecio)){
+        console.log("si paso la validacion");
+        alerta.className = "alert alert-danger my-3 d-none";
+    }else{
+        console.log("no paso la validacion")
+        alerta.className = "alert alert-danger my-3";
     }
 }
 
@@ -47,9 +66,11 @@ let campoProducto = document.querySelector("#producto");
 let campoDescripcion = document.querySelector("#descripcion");
 let campoURL = document.querySelector ("#url");
 let campoPrecio = document.querySelector("#precio");
+let formularioProductos = document.querySelector("#formProductos");
 
 
 campoProducto.addEventListener("blur", function(){campoRequerido(campoProducto)});
 campoDescripcion.addEventListener("blur", ()=>{campoRequeridoDescripcion(campoDescripcion)});
 campoURL.addEventListener("blur", ()=>{ValidarURL(campoURL)});
 campoPrecio.addEventListener("blur", ()=>{ValidarNumeros(campoPrecio)});
+formularioProductos.addEventListener("submit", ValidarGeneral);
