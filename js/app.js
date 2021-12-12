909f254d0d2a5d894b6d83d164cb91475aa001ed
Ezequiel Garcia <ezequielgar@gmail.com>
2021-12-12 16:59:21 -0300

    se crea rama para realaciones de pagina principal y stock

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// traer los datos del localStorage
+let listaProductos = JSON.parse (localStorage.getItem('listaProductosKey')) || [];
+listaProductos.forEach((producto)=>{
+    crearCard(producto);
+});
+function crearCard(producto){
+    let grilla = document.getElementById('grillaPrincipal');
+    grilla.innerHTML += `<div class="col-sm-12 col-md-4 text-center ">
+
+    <div class="card" style="width: 18rem;">
+        <img src="${producto.url}" class="card-img-top" alt="${producto.producto} ">
+        <div class="card-body">
+          <h5 class="card-title">${producto.producto} </h5>
+          <p class="card-text">${producto.descripcion} </p>
+          <p class="card-text">$ ${producto.precio} </p>
+          
+        </div>
+      </div>
+</div>`
+    
+
+}
+
